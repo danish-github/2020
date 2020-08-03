5ff3256f207691f0a743107cb077e7375eb36ead
danish <er_danish@ymail.com>
2020-08-03 10:12:30 +0530

    updt

---
+++ playground/public/scripts/2_fizzBuz_divisibleBy3_5.js
@@ -0,0 +1,8 @@
+for ( i =1; i < 101 ; i++)
+    if ( i % 3 ==0 && i % 5 == 0 )
+     console.log("fizBuzz",i);
+     else if ( i % 3 ==0 && i % 5 !== 0 )
+      console.log("fizz",i);
+    else if ( i % 3 !==0 && i % 5 == 0 )
+     console.log("buzz",i);
+    
---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+let numb = Number(prompt("give a number"));
+
+for (i = 1 ; i <= numb ; i ++) {
+    if (i % 2 == 0)
+    console.log("# # # #");
+    else
+    console.log(" # # # #");
+
+}
